3d92d26bedcf486b8ff41749a1ba3d4ebc316d7f
Jen Ayala <jenayala1@gmail.com>
2017-07-19 09:45:05 -0500

    Completion of if_else.php exercise

--- if_else.php
+++ if_else.php
@@ -7,20 +7,24 @@ $b = 10;
 $c = '10';
 
 // TODO: Shorten these two if statements to a single if/else
-if ($a < $b) {
-    echo "$a is less than $b\n";
-}
-if ($a >= $b) {
-    echo "$a is greater than or equal to $b\n";
-}
+// if ($a < $b) {
+//     echo "$a is less than $b\n";
+// }
+// if ($a >= $b) {
+//     echo "$a is greater than or equal to $b\n";
+// }
+
+echo $a < $b ? "$a is less than $b\n" : "$a is greater than or equal to $b\n";
 
 // TODO: Shorten these two if statements to a single if/else
-if ($b < $c) {
-    echo "$b is less than $c\n";
-}
-if ($b >= $c) {
-    echo "$b is greater than or equal to $c\n";
-}
+// if ($b < $c) {
+//     echo "$b is less than $c\n";
+// }
+// if ($b >= $c) {
+//     echo "$b is greater than or equal to $c\n";
+// }
+
+echo $b < $c ? "$b is less than $c\n" : "$b is greater than or equal to $c\n";
 
 // TODO:
 // combine the next 3 conditionals into one
@@ -29,9 +33,13 @@ if ($b >= $c) {
 if ($b === $c) {
     echo "$b is identical to $c\n";
 }
-if ($b == $c) {
+elseif ($b == $c) {
     echo "$b is equal to $c\n";
 }
-if ($b != $c) {
+else {
     echo "$b is not equal to $c\n";
 }
+
+
+
+
