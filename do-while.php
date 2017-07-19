19a9dbd2210968a390be55b7ffa3871b8b0a8733
Jen Ayala <jenayala1@gmail.com>
2017-07-19 10:31:01 -0500

    Corrections to while & completion of do-while exercise

--- do-while.php
+++ do-while.php
@@ -1,2 +1,28 @@
 <?php
 
+//EXERCISE #1
+$a = 0;
+
+do {
+	echo $a .PHP_EOL;
+	$a += 2;
+	
+} while ($a <= 100);
+
+
+//EXERCISE #2
+$a = 100;
+do {
+	echo $a .PHP_EOL;
+	$a -= 5;
+	
+} while ($a >= -10);
+
+//EXERCISE #3
+$a = 2;
+
+do {
+	echo $a .PHP_EOL;
+	$a *= $a;
+	
+} while ($a < 1000000);
--- while.php
+++ while.php
@@ -4,5 +4,5 @@ $test = 5;
 
 while ($test <=15) {
 	echo "\$test is equal to {$test}\n";
-	$test++;
+	$test++; //can also use $test+=1; to increment by 1
 }
