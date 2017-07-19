a4b11c70365040ee7729878cbb6f441609d1d223
Jen Ayala <jenayala1@gmail.com>
2017-07-19 10:12:34 -0500

    Addiitonal corrections to if and, missing last steps

--- if_and_or.php
+++ if_and_or.php
@@ -28,6 +28,18 @@ if (0 < $z || $z < 10) {
 // TODO:
 // If 0 is less than $x AND $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} AND {$x} is less than 10".
+if (0 < $x && $x <10) {
+	echo "0 is less than {$x} AND {$x} is less than 10";
+}
 
 // TODO:
 // repeat the if statement for $y and $z.
+if (0 < $y && $y <10) {
+	echo "0 is less than {$y} AND {$y} is less than 10";
+}
+
+if (0 < $z && $z <10) {
+	echo "0 is less than {$z} AND {$z} is less than 10";
+}
+
+
