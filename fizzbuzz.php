43cdcf89280292f65c66ad2c59db747b6d21fe3f
Jen Ayala <jenayala1@gmail.com>
2017-07-20 11:44:12 -0500

    Revision to fizzbuzz to include if divisible by both, changed to else if

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,16 +3,21 @@
 
 for ($number = 1; $number <= 100; $number++) {
 
-	if (0 == ($number % 3)){
+	if (0 == ($number % 3) && ($number % 5)) {
+		echo  "fizzbuzz" . PHP_EOL;
+	}
+
+	else if (0 == ($number % 3)){
 		echo "fizz" . PHP_EOL; 
 	}
 
-	if (0 == ($number % 5)){
-		print "buzz" . PHP_EOL;
-	} 
+	else if (0 == ($number % 5)){
+		echo "buzz" . PHP_EOL;
+	} 	
 
-	if (0 != ($number % 3) || ($number % 5)) {
-		 echo "$number" . PHP_EOL;
+	else  {
+		echo  "$number" . PHP_EOL;
 	}
 
 }
+
