6c9013bd78e3c91bf8f90a8990fe849a4b4dfa7b
Jen Ayala <jenayala1@gmail.com>
2017-07-20 10:55:02 -0500

    Completion of for loop exercise

--- for.php
+++ for.php
@@ -2,19 +2,33 @@
 
 
 
-fwrite(STDOUT, "Please enter a starting number");
-
+fwrite(STDOUT, "Please enter a starting number: ");
 $startingNumber = trim(fgets(STDIN));
+if (! is_numeric($startingNumber)){
+		echo "That is not a number " . PHP_EOL;
+		exit;
+}
 
-fwrite(STDOUT, "Please enter an ending number");
-
+fwrite(STDOUT, "Please enter an ending number: ");
 $endingNumber = trim(fgets(STDIN));
+if (! is_numeric($endingNumber)){
+		echo "That is not a number " . PHP_EOL;
+		exit;
+	}
 
-var_dump($startingNumber);
-var_dump($endingNumber);
+fwrite(STDOUT, "Please enter increment: ");
+$increment = trim(fgets(STDIN));
+if (! is_numeric($increment)){
+		echo "That is not a number " . PHP_EOL;
+		exit;
+	}
+
+if ($increment == "") {
+	$increment = 1;
+}
 
 
-for ($ = $startingNumber; $i < $endingNumber; $i++){
+for ($i = $startingNumber; $i < $endingNumber; $i+=$increment){
 	echo $i . PHP_EOL;
 
 }
