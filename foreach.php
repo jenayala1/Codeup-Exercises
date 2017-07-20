12aa6e46b7cabfd0f90a84e30bb1289113c1f5ab
Jen Ayala <jenayala1@gmail.com>
2017-07-20 14:09:41 -0500

    Exercises 1-3 of foreach PHP

--- foreach.php
+++ foreach.php
@@ -4,21 +4,34 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $item){
 
-	if (is_array($item)) {
-		echo "The array $item" . PHP_EOL;
-	}
+// 	if (is_array($item)) {
+// 		echo "$item is an array" . PHP_EOL;
+// 	}
 
-		else if (is_int($item)) {
-				echo "$item are numbers" . PHP_EOL;
-		} 
+// 		else if (is_int($item)) {
+// 			echo "$item are numbers" . PHP_EOL;
+// 		} 
 
-		else if (is_float($item)) {
-		  	echo "$item is a float" . PHP_EOL;
-		}
+// 		else if (is_float($item)) {
+// 		  	echo "$item is a float" . PHP_EOL;
+// 		}
 
-		else if (is_bool($item)) {
-		echo "$item is boolean" . PHP_EOL;
-		}
+// 		else if (is_bool($item)) {
+// 			echo "$item is boolean" . PHP_EOL;
+// 		}
 
-		else if
+// 		else if (is_null($item)) {
+// 			echo "$item is null" . PHP_EOL;
+// 		}
+
+// 		else if(is_string($item)) {
+// 			echo "$item is a string" . PHP_EOL;
+// 		}
+	// if (is_scalar($item)){
+	// 	echo "$item" . PHP_EOL;
+	// }
+
+
+
 }
+
