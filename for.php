bf87eda327c8d638d7248339d188460300a80472
Jen Ayala <jenayala1@gmail.com>
2017-07-20 12:08:12 -0500

    foreach lesson notes & start to foreach exercise

--- for.php
+++ for.php
@@ -32,3 +32,32 @@ for ($i = $startingNumber; $i < $endingNumber; $i+=$increment){
 	echo $i . PHP_EOL;
 
 }
+
+///FOR EACH LOOP
+//count performs the same function as .length in JS
+//EXAMPLE: 
+
+$numbers = ['zero', 'one', 'two', 'three', 'four', 'five'];
+
+for ($i = 0; $i < count($numbers); $i++) {
+    echo "\$numbers has an element with a value of {$numbers[$i]}\n";
+}
+
+//REWRITE in FOR EACH: ("as" identifies the array index items )
+foreach ($numbers as $number) {
+	echo 'the number is: ' . $number . "!" .PHP_EOL;
+}
+
+$animalTypes = array('dogs', 'cats', 'birds', 'narwhals');
+
+foreach ($animalTypes as $animalType){
+	echo 'the animal type is: ' .$animalType . '!' . PHP_EOL;
+}
+
+
+
+
+
+
+
+
---
+++ foreach.php
@@ -0,0 +1,11 @@
+<?php
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $thing){
+	echo "The array is " . $thing . PHP_EOL;
+
+	// if (is_array()) {
+	// 	echo 
+	// }
+}
