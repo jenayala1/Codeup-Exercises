9b188164ae80746582662e1766e3189cfd491854
Jen Ayala <jenayala1@gmail.com>
2017-07-21 12:21:44 -0500

    Step #2 completed for break continue exercise

--- break_continue.php
+++ break_continue.php
@@ -10,4 +10,11 @@ for ($i = 1; $i <= 100; $i++) {
 }
 
 
-
+//#2 - Create another for loop that counts from 1 to 100, but stops after 10 using break
+for ($i = 1; $i <= 100; $i++) {
+    
+    if ($i == 11) {
+        break;
+    }
+    echo $i . PHP_EOL;
+}
