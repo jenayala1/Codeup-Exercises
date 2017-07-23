45e426c32629f6a80063932aee91d81def5927c9
Jen Ayala <jenayala1@gmail.com>
2017-07-23 15:42:50 -0500

    Functions written for each arithmetic statement, still not echoing results

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,65 @@
 <?php
 
-function add($a, $b)
-{
-    return $a + $b;
+$a = 4;
+$b = 25;
+
+function inputsAreNumeric($a, $b) {
+// ====== // Add code here // =========== //
+	if (is_numeric($a) && is_numeric($b)) {
+		echo true;
+	} else {
+		echo false;
+	}
 }
 
-function subtract($a, $b)
-{
-    // Add code here
+function add($a, $b) {
+// ====== // Add code here // =========== //
+  	if (is_numeric($a) && is_numeric($b)) {
+  		return $a + $b;
+		echo $a + $b . PHP_EOL;
+	} else {
+		return "Input must be numeric";
+		echo "Inputs must be numeric" . PHP_EOL;
+	}
 }
 
-function multiply($a, $b)
-{
-    // Add code here
+
+function subtract($a, $b) {
+// ====== // Add code here // =========== //
+	if (is_numeric($a) && is_numeric($b)) {
+  		return $a - $b;
+		echo $a - $b . PHP_EOL;
+	} else {
+		return "Inputs must be numeric";
+		echo "Inputs must be numeric" . PHP_EOL;
+	}
 }
 
-function divide($a, $b)
-{
-    // Add code here
+function multiply($a, $b) {
+// ====== // Add code here // =========== //
+	if (is_numeric($a) && is_numeric($b)) {
+	  		return $a * $b;
+			echo $a * $b . PHP_EOL;
+	} 	else {
+			return "Inputs must be numeric";
+			echo "Inputs must be numeric" . PHP_EOL;
+		}
+}
+
+function divide($a, $b) {
+// ====== // Add code here // =========== //
+	if (is_numeric($a) && is_numeric($b)) {
+	  		return $a / $b;
+			echo $a / $b . PHP_EOL;
+	} 	else {
+			return "Inputs must be numeric";
+			echo "Inputs must be numeric" . PHP_EOL;
+		}
+
 }
 
 // Add code to test your functions here
+
+
+
+
