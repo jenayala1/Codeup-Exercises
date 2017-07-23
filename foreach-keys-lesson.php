f2353e1ab713d9e1538ab56128e9caa6539805d0
Jen Ayala <jenayala1@gmail.com>
2017-07-23 15:46:23 -0500

    Updated notes

--- foreach-keys-lesson.php
+++ foreach-keys-lesson.php
@@ -57,7 +57,64 @@ foreach ($students as $student) {
 }
 
 
+//BONUS EXERCISES:
 
+<?php 
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    ), 
+    'Lord of the Rings' => array(
+        'published' => 1968,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 1178
+    )
+);
+
+
+// BONUS 3 - output all first entries by an author given an array of books information
+
+// !!!DISCLAIMER!!! this bonus exercise is much more challenging than originally estimated without using array functions in PHP.
+// The following solution only uses existing code we have used in the curriculum up to this lesson to solve this bonus question. A simpler solution would involve creating a new array of author names using array_push().
+
+$parentIndex = -1; // variable to track current numeric index in parent loop traversal of an associative array
+foreach($books as $key => $book) { // iteratate over books array
+    $parentIndex++; // increment index of parent loop
+    $matchingRecords = 0; // variable to keep track of any matching author records
+    $childIndex= -1; // variable to track the current numeric index of the child loop traversal of an associative array
+    foreach($books as $someKey => $someBook) { // child loop to iterate over the entire books array to check before an output is made
+        $childIndex++; // increment index of child loop
+        if ($book['author'] == $someBook['author']) { // checking only if author matches between parent loop and child loop
+            $matchingRecords++; // if inner and outer loop authors match, increment the match count
+            if ($matchingRecords == 1 && $parentIndex == $childIndex) { // if the matchcount is 1 and the inner and outer indexes match...
+                // ... output the record
+                echo $key . PHP_EOL;
+                foreach($book as $header => $data) {
+                    echo "$header : $data" . PHP_EOL;
+                }
+                echo "====================\n";
+            }
+        }
+    }
+}
 
 
 
