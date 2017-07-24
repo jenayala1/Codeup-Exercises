76deb0f32a777edcfb364fad0e278977ef691869
Jen Ayala <jenayala1@gmail.com>
2017-07-24 13:54:15 -0500

    Array Sorting Exercise: Additional correction to step #4, moving to step #5

--- technology-companies.php
+++ technology-companies.php
@@ -61,17 +61,18 @@ $companies = [
 //Sort the people in each company alphabetically. 
 //Use a foreach loop and will need to reassign each inner array after sorting. Output the result.
 foreach ($companies as $company => $people) {
-    ksort($people);
+    natsort($people);
+    sort($people);
     print_r($company) . print_r($people);
 }
 
 
 // ==== STEP #5 ====
 //Sort the companies from "biggest" to "smallest". This may be easier than you think, but be sure you don't loose the company names!
-// foreach ($companies as $company => $people) {
+foreach ($companies as $company => $people) {
      
-//      print_r($company) . print_r($people);
-// }
+      print_r($company) . print_r($people);
+}
 
 
 
