06ac9ae1eddcee800469bc184a8f6cb9a8d1ff0c
Jen Ayala <jenayala1@gmail.com>
2017-07-25 14:42:41 -0500

    Continued work on list with commas, creation of php library file

---
+++ library.php
@@ -0,0 +1,52 @@
+<?php
+
+// ===# Make your own PHP library of helpful functions! Make a file called library.php == //
+
+// Write a function called isEven that takes in a variable and returns true or false if the provided input is evenly divisible by two or not.
+function isEven($var){
+	if ($var % 2 == 0) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+
+//Write a function called isVowel that returns true or false if the provided input is the letter 'a', 'e', 'i', 'o', or 'u'
+function isVowel(){
+
+
+}
+
+//Write a function called "first" that takes in an argument that could be an array or a string. Return the first character if the argument is a string. Return the first element of the array if the input is an array.
+function first(){
+
+}
+
+
+//Write a function called "second" that takes in input that could be an array or a string. The function should return the second character or element of the input.
+function second(){
+
+
+}
+
+//Write a function called "last" that takes in an input that could be an array or a string. Your function should return the last character or element of the input.
+function last(){
+
+}
+
+
+//Write a function called "reverse" that takes in an input of either a string or an array. Your function should return the elements or characters in reverse order.
+function reverse(){
+
+}
+
+
+//Write a function called "random" that takes in an input that can be either a string or an array. Your function should return a random element or character from the input.
+function random(){
+
+}
+
+
+
--- list_with_commas.php
+++ list_with_commas.php
@@ -1,12 +1,31 @@
 <?php
 
+//STEPS 1 & 3:
+
+// $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+// $physicsArray = explode(', ', $physicistsString);
+// sort($physicsArray);
+// $lastItem = array_pop($physicsArray);
+// $newPhysicsArray = array_push($physicsArray, 'and ' . $lastItem);
+// $famousFakePhysicists = implode(',', $physicsArray);
+
+// echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
+
+//=========================================
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+	$array = explode(', ', $physicistsString);
 
-$physicsArray = explode(', ', $physicistsString);
-sort($physicsArray);
-$lastItem = array_pop($physicsArray);
-$newPhysicsArray = array_push($physicsArray, 'and ', $lastItem);
+function humanizedList($array, $sort = false) {
+	$lastItem = array_pop($array);
+	$physicsArray = array_push($array, 'and ' . $lastItem); 
+
+	 	if ($sort = true){
+		 	asort($array);
+		} 
+
 $famousFakePhysicists = implode(',', $physicsArray);
-
-
+return $famousFakePhysicists;	
+}
+humanizedList($array, false);
+//return $famousFakePhysicists;	
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
