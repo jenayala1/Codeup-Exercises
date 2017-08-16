283ce63b5c14f528be87eb83e547eb66ba270178
Jen Ayala <jenayala1@gmail.com>
2017-08-16 11:45:51 -0500

    Corrections to square/override exercise to follow instructor solutions

--- Square.php
+++ Square.php
@@ -5,10 +5,15 @@ require_once "Rectangle.php";
 class Square extends Rectangle
 {
 
-	public function __construct($height, $width)
+	public function __construct($height)
 	{
+		//Update the Square class to override the parent's constructor by only accepting one attribute (height) and using that to set both $width and $height in your class.
+		//$this->height = $height;
+		//$this->width = $height;
+
+		//Update the constructor in Square to instead pass height on to the parent's constructor.
+		$width = $height;
 		parent::__construct($height, $width);
-		$this->width = $width;
 	}
 
 	public function perimeter()
@@ -16,9 +21,4 @@ class Square extends Rectangle
 		return ( 2 * ($this->height)) + (2 * ($this->width));
 	}
 
-	public function area()
-	{
-		return $this->height * $this->width;
-	}
-		
 }
--- shapes_test.php
+++ shapes_test.php
@@ -3,8 +3,8 @@
 require_once "Rectangle.php";
 require_once "Square.php";
 
-$rectangle1 = new Rectangle(2, 5);
-echo "A rectangle with this height and weight has an area of " . $rectangle1->area();
+$rectangle1 = new Rectangle(3, 10);
+echo "A rectangle with this height and weight has an area of " . $rectangle1->area() . " and perimeter of " . $rectangle1->perimeter() . PHP_EOL;
 
-$square1 = new Square(2, 2);
+$square1 = new Square(4, 4);
 echo "A square with this height and weight has an area of " . $square1->area() . " and perimeter of " . $square1->perimeter() . PHP_EOL;
